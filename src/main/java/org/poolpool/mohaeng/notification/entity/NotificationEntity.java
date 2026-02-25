e75418a408a739e13e0de7377971aa8e94109c7c
mj <alswp1225@gmail.com>
2026-02-25 16:42:36 +0900

    refactor)notificationentity 코드 수정

--- src/main/java/org/poolpool/mohaeng/notification/entity/NotificationEntity.java
+++ src/main/java/org/poolpool/mohaeng/notification/entity/NotificationEntity.java
@@ -16,28 +16,35 @@ public class NotificationEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "NOTIFICATION_ID")
     private Long notificationId;
 
-    @Column(nullable = false)
+    @Column(name = "USER_ID", nullable = false)
     private Long userId;
 
-    @Column(nullable = false)
+    //  DB 컬럼명: NOTITYPE_ID
+    @Column(name = "NOTITYPE_ID", nullable = false)
     private Long notiTypeId;
 
+    @Column(name = "EVENT_ID")
     private Long eventId;
+
+    @Column(name = "REPORT_ID")
     private Long reportId;
 
-    // 중복방지 키/추가 상태 저장 용도
-    @Column(length = 50)
+    @Column(name = "STATUS1", length = 50)
     private String status1;
 
-    @Column(length = 50)
+    @Column(name = "STATUS2", length = 50)
     private String status2;
 
+    @Column(name = "EVENT_BEFORE_AT")
     private LocalDateTime eventBeforeAt;
+
+    @Column(name = "EVENT_DAY_AT")
     private LocalDateTime eventDayAt;
 
-    @Column(nullable = false)
+    @Column(name = "CREATED_AT", nullable = false)
     private LocalDateTime createdAt;
 
     @PrePersist
