61c73ec0c40660000bd5bac549cbf6b325e95a83
mj <alswp1225@gmail.com>
2026-02-27 18:19:16 +0900

    refactor) report 컨트롤러 수정

--- src/main/java/org/poolpool/mohaeng/event/report/controller/EventReportController.java
+++ src/main/java/org/poolpool/mohaeng/event/report/controller/EventReportController.java
@@ -6,6 +6,7 @@ import org.poolpool.mohaeng.admin.report.dto.AdminReportCreateRequestDto;
 import org.poolpool.mohaeng.admin.report.service.AdminReportService;
 import org.poolpool.mohaeng.common.api.ApiResponse;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import lombok.RequiredArgsConstructor;
@@ -19,10 +20,10 @@ public class EventReportController {
 
     @PostMapping
     public ResponseEntity<ApiResponse<Void>> create(
-        @RequestHeader(name = "userId") long reporterId,
+        @AuthenticationPrincipal String userId,
         @Valid @RequestBody AdminReportCreateRequestDto request
     ) {
-        reportService.create(reporterId, request);
+        reportService.create(Long.valueOf(userId), request);
         return ResponseEntity.ok(ApiResponse.ok("이벤트 신고 등록 성공", null));
     }
 }
